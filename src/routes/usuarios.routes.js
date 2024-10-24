c1b9e4becb110cd5c0876bc434df1747ea7e4f75
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-10-24 16:55:12 -0300

    feat: get create  all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,12 @@ const usuariosRoutes = Router();
 
 let usuarios = [];
 
+usuariosRoutes.get("/", (req, res) => {
+  return res.status(200).json({
+    message: usuarios.length == 0 ? "Nenhum usuário cadastrado" : `Total de usuários: ${usuarios.length}`,
+    usuarios,
+  }
+  );
+});
+
 export default usuariosRoutes;
